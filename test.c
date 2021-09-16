a3a1ca4d803f8dcc57538818b17dbe537b367e53
AlexMuellr <alexanderml587@gmail.com>
2021-09-16 18:28:39 +0200

    basic number add from input

--- test.c
+++ test.c
@@ -1,5 +1,24 @@
 #include <stdio.h>
 
-int main(){
-  printf("HI\n");
+
+
+int main()
+{
+  int num1;
+  int num2;
+  int num3;
+
+  int result = num1 + num2 + num3;
+
+
+  printf("Enter a number\n");
+  scanf("%f",  &num1);
+
+  printf("Enter another number\n");
+  scanf("%f",  &num2);
+
+  printf("Enter a number\n");
+  scanf("%f",  &num3);
+  printf("%s\n", result);
+  return 0;
 }
